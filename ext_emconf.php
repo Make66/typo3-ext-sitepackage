f306b05588c3eda12eebe8f1b07e7851eae0f5ff
Martin Keller <martin.keller@taketool.de>
2026-07-09 10:28:22 +0200

    [sitepackage] start dev for v12+

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-14.5.99',
             'container' => '1.0.0-2.9.99',
         ],
         'conflicts' => [],
